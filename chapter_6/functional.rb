a4c0dd89b33d3bce1f3c4a2a55bf50d3330b0c8b
Edgar Villamarin <edgarv.uribe@hotmail.com>
2022-10-24 10:36:35 -0500

    chapter 7 functional programming

--- chapter_6/functional.rb
+++ chapter_6/functional.rb
@@ -41,3 +41,60 @@ def functional_singles(states)
   states.select { |state| state.split.length == 1 }
 end
 puts functional_singles(states).inspect
+
+numbers = 1..10
+
+# sum: Imperative solution
+def imperative_sum(numbers)
+  total = 0
+  numbers.each do |n|
+    total += n
+  end
+  total
+end
+puts imperative_sum(numbers)
+
+# sum: Functional solution
+def functional_sum(numbers)
+  numbers.reduce { |total, n| total + n }
+end
+puts functional_sum(numbers)
+
+# lengths: Imperative version
+def imperative_lengths(states)
+  lengths = {}
+  states.each do |state|
+    lengths[state] = state.length
+  end
+  lengths
+end
+puts imperative_lengths(states)
+
+# lengths: Functional version
+def functional_lengths(states)
+  states.reduce({}) do |lengths, state|
+    lengths[state] = state.length
+    lengths
+  end
+end
+puts functional_lengths(states)
+
+# lengths: Functional version using `reduce` `merge`
+# puts states.reduce({}) { |lengths, state| lengths.merge({state => state.length}) }
+
+def functional_lengths(states)
+  states.inject({}) do |lengths, state|
+    lengths.merge({state => state.length})
+  end
+end
+puts functional_lengths(states)
+
+
+# lengths: Functional version using `inject`
+def functional_lengths(states)
+  states.inject({}) do |lengths, state|
+    lengths.merge({state => state.length})
+  end
+end
+puts functional_lengths(states)
+
